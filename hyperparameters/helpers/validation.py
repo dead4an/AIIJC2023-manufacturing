b5beeeb302a5ca07ccaab4dc45e98ac36822e938
Dead4an <linker4an@mail.ru>
2023-09-24 19:57:46 +0300

    code clean

---
+++ hyperparameters/helpers/validation.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, classification_report, f1_score, roc_auc_score, make_scorer
+
+
+def valid_predictions(y_true, y_predicted) -> None:
+    # Строим матрицу ошибок
+    cm = confusion_matrix(y_true, y_predicted)
+    cm = pd.DataFrame(data=cm, columns=['0', '1'],  
+                             index=['0', '1'])
+    
+    # Отображение матрицы ошибок
+    sns.heatmap(cm, annot=True, fmt='d', cmap='YlGnBu')
+    plt.show()
+
+    # Отчёт по классификации
+    print(classification_report(y_true, y_predicted, digits=5))
+
+    # Получаем F1 и ROC AUC метрики
+    print(f"F1-macro: {f1_score(y_true, y_predicted, average='macro')}")
+    print(f'ROC-AUC score: {roc_auc_score(y_true, y_predicted)}')
